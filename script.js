a439ccc5e59e2c2ea6a8e1698d9a0912f6308bcd
Akshatha Achar <95159069+Akshatha-Achar@users.noreply.github.com>
2022-12-05 19:02:46 +0530

    Functionalities to add content to the topic selected

--- script.js
+++ script.js
@@ -8,9 +8,15 @@ const insideBookSection = document.querySelector(".inside-book-section");
 const openBookButton = document.querySelector("#open-book");
 const insideBookContainer = document.querySelector(".inside-book-container");
 const bookTitle = document.querySelector(".book-title");
+const topicTitle = document.querySelector(".topic-title");
+
 const topicsContainer = document.querySelector(".topics-container");
 const addTopicButton = document.querySelector(".add-topic");
 const whiteSheet = document.querySelector(".white-sheet");
+const deleteTopicButton = document.querySelector(".delete-topic");
+
+//***************************************************
+// LANDING PAGE
 
 openButton.addEventListener("click", function () {
   landingPage.classList.add("hidden");
@@ -29,7 +35,7 @@ getAllBooks().forEach((book) => {
 
 function addBooks() {
   const books = getAllBooks();
-  const bookNameValue = prompt("What is the book name:");
+  const bookNameValue = prompt("What is the book name:").trim();
   if (bookNameValue) {
     const bookObject = {
       id: Math.floor(Math.random() * 10000),
@@ -61,7 +67,6 @@ function createBook(id, bookName) {
       deleteBook(id, element);
     }
   });
-
   return element;
 }
 
@@ -81,7 +86,7 @@ function deleteBook(id, book) {
 openBookButton.addEventListener("click", openBook);
 
 function openBook() {
-  const bookNameInput = prompt("Write the book Name to open:");
+  const bookNameInput = prompt("Write the book Name to open:").trim();
   const book = getTheBook(bookNameInput);
 
   if (book) {
@@ -98,9 +103,27 @@ function openBook() {
       topicsContainer.insertBefore(topicElement, addTopicButton);
     });
   } else {
-    alert(`' ${bookNameInput} ' book doesn't exist. Try Again...☹`);
+    alert(`"${bookNameInput}" book doesn't exist. Try Again..😓`);
   }
 }
+
+//******************* */
+// for Testing
+// const bookNameInput = "Maths";
+// const book = getTheBook(bookNameInput);
+// allBooksSection.classList.add("hidden");
+// insideBookSection.classList.remove("hidden");
+// bookTitle.innerText = book.bookName;
+
+// book["topics"].forEach((topic) => {
+//   const topicElement = createTopic(
+//     book.bookName,
+//     topic.topicId,
+//     topic.topicName
+//   );
+//   topicsContainer.insertBefore(topicElement, addTopicButton);
+// });
+//****************** */
 
 function getTheBook(bookName) {
   const book = getAllBooks().filter((book) => book.bookName === bookName)[0];
@@ -149,20 +172,24 @@ function createTopic(bookName, topicId, topicName) {
   element.value = topicName;
 
   element.addEventListener("change", function (e) {
-    const newTopicName = e.target.value;
+    console.log(e.target.value);
+    const newTopicName = e.target.value.trim();
     updateTopic(bookName, topicId, newTopicName);
   });
 
-  element.addEventListener("dblclick", function (e) {
-    if (e.ctrlKey) {
-      const doDelete = confirm("Do you want to delete this topic");
-      if (doDelete) {
-        deleteTopic(bookName, topicId, element);
-      }
-    }
-  });
   element.addEventListener("click", function (e) {
+    topicTitle.innerText = e.target.value;
+    const clickedElements = document.querySelectorAll(".topic-selected");
+    if (clickedElements !== []) {
+      clickedElements.forEach((element) =>
+        element.classList.remove("topic-selected")
+      );
+    }
     e.target.classList.toggle("topic-selected");
+    whiteSheet.value = getTheBook(bookName).topics.filter(
+      (topic) => topic.topicId === topicId
+    )[0].content;
+    console.log(`Topic-Name: ${element.value}\n Content: ${whiteSheet.value}`);
   });
   return element;
 }
@@ -176,28 +203,51 @@ function updateTopic(bookName, topicId, newTopicName) {
   console.log(bookName, topics);
 }
 
-function deleteTopic(bookName, topicId, topicElement) {
-  const topics = getTheBook(bookName).topics;
-  const remainTopics = topics.filter((topic) => topic.topicId !== topicId);
-  saveTopics(bookName, remainTopics);
-  topicsContainer.removeChild(topicElement);
-  console.log(bookName, remainTopics);
-}
-
 //WHEN THE TOPIC NAME IS SELECTED TO WRITE ITS CONTENT
 whiteSheet.addEventListener("change", function (e) {
-  console.log(e);
-  const bookName = bookTitle.innerText;
-  const newContent = whiteSheet.innerText;
-  updateContent(bookName, topicId, newContent);
+  const selectedElement = topicsContainer.querySelector(".topic-selected");
+  console.log(selectedElement);
+  if (!selectedElement) {
+    alert("Please select the topic Name form the topic list...");
+  } else {
+    const bookName = bookTitle.innerText;
+    const topicName = selectedElement.value;
+    const newContent = e.target.value.trimEnd();
+    console.log(getTheBook(bookName).topics);
+    updateContent(bookName, topicName, newContent);
+    console.log(`Topic-Name: ${topicName}\n NewContent: ${newContent}`);
+  }
 });
 
-function updateContent(bookName, topicId, newContent) {
+function updateContent(bookName, topicName, newContent) {
   const topics = getTheBook(bookName).topics;
-  const targetTopic = topics.filter((topic) => topic.topicId === topicId);
+  const targetTopic = topics.filter((topic) => topic.topicName === topicName);
   targetTopic[0]["content"] = newContent;
-  topicsContainer.querySelector(`.topic-${topicId}`).style.backgroundColor =
-    "blue";
-  console.log(bookName, targetTopic);
-  // saveTopics(bookName, topics);
+  saveTopics(bookName, topics);
+}
+
+//WHEN YOU CLICK DELETE TOPIC BUTTON
+
+deleteTopicButton.addEventListener("click", deleteTopic);
+
+function deleteTopic() {
+  const bookName = bookTitle.innerText;
+  const topicNameUI = prompt("Write the Topic Name to DELETE:").trim();
+  console.log(topicNameUI);
+  const topics = getTheBook(bookName).topics;
+  const topicRequired = topics.filter(
+    (topic) => topic["topicName"] === topicNameUI
+  );
+  if (!topicRequired[0]) {
+    alert("Incorrect Topic Name...Try again...");
+  } else {
+    const id = topicRequired[0].topicId;
+    const topicElement = topicsContainer.querySelector(`.topic-${id}`);
+    const remainTopics = topics.filter((topic) => topic.topicId !== id);
+
+    saveTopics(bookName, remainTopics);
+    topicsContainer.removeChild(topicElement);
+    whiteSheet.value = "";
+    topicTitle.innerText = "";
+  }
 }
